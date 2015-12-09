ccabf9ad7dcc585106b4024978378806a1f3915a
Microfed <microfed@gmail.com>
2015-12-09 18:18:15 +0300

    Clean folder

--- app/mirage/factories/project.js
+++
@@ -1,7 +0,0 @@
-import Mirage, {faker}  from 'ember-cli-mirage';
-
-export default Mirage.Factory.extend({
-   title: faker.company.companyName,
-   description: faker.lorem.paragraph,
-   id: faker.random.uuid
-});
--- app/routes/project.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  afterModel(model) {
-    this.transitionTo('project.builds', model);
-  }
-});
